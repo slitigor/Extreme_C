8069936b3b2d5e6226a4e18a2b99e02230793e49
slitigor <slt-ig@rambler.ru>
2022-03-21 16:53:47 +0500

    Added 'example1_9.c' file

---
+++ Chapter1/example1_9.c
@@ -0,0 +1,16 @@
+// Файл example1_5.c 
+// Книга "Extreme C" Amini K. Часть 1. Указатели
+// Пример объявления и использования указателей
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	int var = 100;
+	int* ptr = 0;
+	printf("Volume 'var' = %d\n", var);
+	ptr = &var;
+	*ptr = 200;
+	printf("Volume 'var' = %d\n", var);
+
+	return 0;
+}
