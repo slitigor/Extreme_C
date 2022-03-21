1f999815cb81499b984db17df70e5842c651aca8
slitigor <slt-ig@rambler.ru>
2022-03-21 20:25:39 +0500

    Added 'example1_10.c' file

---
+++ Chapter1/example1_10.c
@@ -0,0 +1,24 @@
+// ‘айл example1_5.c 
+//  нига "Extreme C" Amini K. „асть 1. ”казатели
+// ƒлина арифметического шага дл€ двух указателей
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	int var = 1;
+	int* int_ptr = NULL;	// ќбъ€вили и обнулили (об€зательно!) указатель
+	int_ptr = &var;
+	
+	char* char_ptr = NULL;
+	char_ptr = (char*)&var;
+	
+	printf("Before arithmetic: int_ptr: %u; char_ptr: %u\n", 
+			(unsigned int)int_ptr, (unsigned int)char_ptr);
+	int_ptr++;
+	char_ptr++;
+	printf("After arithmetic: int_ptr: %u; char_ptr: %u\n", 
+			(unsigned int)int_ptr, (unsigned int)char_ptr);
+	
+
+	return 0;
+}
