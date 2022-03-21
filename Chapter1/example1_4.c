a273a4b0f5ae464882ad5397171a59ffb66f53f1
slitigor <slt-ig@rambler.ru>
2022-03-21 15:24:26 +0500

    Added example1_4.c file

---
+++ Chapter1/example1_4.c
@@ -0,0 +1,31 @@
+// Файл example1_4.c 
+// Книга "Extreme C" Amini K. Часть 1. Директивы препроцессора
+// Использование операторов # и ## в макросе
+
+#include <stdio.h>
+#include <string.h>
+
+#define CMD(NAME) \
+	char NAME ## Cmd[256] = ""; \
+	strcpy(NAME ## Cmd, #NAME);
+
+int main(int argc, char** argv) {
+	CMD(copy)
+	CMD(paste)
+	CMD(cut)
+	
+	printf("Ennter your command: ");
+	char cmd[256];
+	scanf("%s", cmd);
+	
+	if (strcmp(cmd, copyCmd) == 0)
+		printf("This enter - 'copy'\n");
+
+	if (strcmp(cmd, pasteCmd) == 0)
+		printf("This enter - 'paste'\n");
+
+	if (strcmp(cmd, cutCmd) == 0)
+		printf("This enter - 'cut'\n");
+
+	return 0;
+}
