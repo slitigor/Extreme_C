45ca5880c5f73b3ea506758f108405052a816197
slitigor <slt-ig@rambler.ru>
2022-04-22 09:27:19 +0500

    Added 'example4_3.c', 'example4_4.c' and modified 'Process memory structure.docx' files

---
+++ Chapter4/example2/example4_2.c
@@ -0,0 +1,12 @@
+// Файл example4_2.c
+// Минимальная программа на C с несколькими неинициализированным
+// и обнулёнными глобальными переменными
+
+int global_var1;
+int global_var2;
+int global_var3 = 0;
+
+int main(int argc, char** argv) {
+
+	return 0;
+}
---
+++ Chapter4/example4_3.c
@@ -0,0 +1,15 @@
+// Файл example4_2.c
+// Минимальная программа на C с инициализированными и
+// неинициализированным глобальными переменными
+
+int 	global_var1;
+int 	global_var2;
+int 	global_var3 = 0;
+
+double 	global_var4 = 4.5;
+char	global_var5 = 'A';
+
+int main(int argc, char** argv) {
+
+	return 0;
+}
